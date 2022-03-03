8d8ef31d301187679ec4bd8379aac8355df0e0b7
James Robertson <digital.robertson@gmail.com>
2022-03-03 09:02:54 +0000

    bug fix: Replaced RXFHelperModule with RXFRead.

--- lib/botbase-module-skillmodel.rb
+++ lib/botbase-module-skillmodel.rb
@@ -10,18 +10,18 @@ require 'alexa_modelbuilder'
 
 
 class BotBaseModuleSkillModel
-  include RXFHelperModule
-  
-  def initialize(file: nil, invocation: nil, userid: nil, callback: nil, 
+  include RXFRead
+
+  def initialize(file: nil, invocation: nil, userid: nil, callback: nil,
                  debug: false)
-    
+
     @invocation, @debug = invocation, debug
     amb = AlexaModelBuilder.new(FileX.read file)
     @skillbot = AskIO.new(amb.to_manifest, amb.to_model, userid: userid)
-    
+
   end
 
-  def query(sender='user01', msg, mode: :voicechat, echo_node: 'node1')    
+  def query(sender='user01', msg, mode: :voicechat, echo_node: 'node1')
     @skillbot.ask msg
   end
 
